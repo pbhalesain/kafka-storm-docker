2e6d5ac287d413942acadb0a3124c9f3ab9284f4
Prashant Bhalesain <pbhalesain@gmail.com>
2018-07-22 11:54:25 +0100

    Stom Base working

---
+++ storm/storm-base/start-supervisor.sh
@@ -0,0 +1,13 @@
+sed -i -e "s/%zookeeper%/$ZK_PORT_2181_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
+sed -i -e "s/%nimbus%/$NIMBUS_PORT_6627_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
+
+echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
+
+echo "supervisor.scheduler.meta:" >> $STORM_HOME/conf/storm.yaml
+echo "    tags: DC1" >> $STORM_HOME/conf/storm.yaml
+echo "preferred.topology.dc.config:" >> $STORM_HOME/conf/storm.yaml
+echo "    tp1: dc1" >> $STORM_HOME/conf/storm.yaml
+echo "    tp2: dc2" >> $STORM_HOME/conf/storm.yaml
+echo "worker.childopts: "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5%ID%" >> $STORM_HOME/conf/storm.yaml"
+
+/usr/sbin/sshd && supervisord
